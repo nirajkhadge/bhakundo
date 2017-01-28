51b00fb4e1b28d1825d1e9ba4cc5c6f5ba151156
Niraj Khadge <Niraj@Nirajs-MBP.attlocal.net>
2017-01-28 13:25:30 -0600

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  root to: "leagues#index"	
   resources :weeks
   resources :games
   resources :teams
   resources :game_types
   resources :leagues
   devise_for :users, :controllers => { registrations: 'registrations' }
-  devise_scope :user do
-  	root to: "devise/sessions#new"
-  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
